d74189d22513a73c1a481abf76ca50e8da49fbe6
Bernardo Santos <bernardosantos670@gmail.com>
2025-03-22 14:18:23 +0000

    open medium links in freedium

--- qutebrowser/.config/qutebrowser/userscripts/launcher.py
+++ qutebrowser/.config/qutebrowser/userscripts/launcher.py
@@ -7,8 +7,11 @@ url = os.getenv('QUTE_URL')
 if "youtube.com/watch" in url:
     url = url.replace("watch", "watch_popup")
     url += "&autoplay=1"
+    command = f":spawn librewolf -P yt-player {url}"
 
-command = f":spawn librewolf -P yt-player {url}"
+elif "medium.com" in url:
+    url = "https://freedium.cfd/" + url
+    command = f":open -t {url}"
 
 with open(os.getenv('QUTE_FIFO'), 'a') as fifo:
     fifo.write(command + '\n')
